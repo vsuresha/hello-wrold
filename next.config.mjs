be7a637998d3a7d103992414da8a1b6046fa4d08
vsuresha <161426274+vsuresha@users.noreply.github.com>
2024-11-04 16:09:08 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/motor-insurance',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
